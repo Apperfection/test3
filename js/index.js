5fd80640098e451e77ae26661b1adcd41e526683
Apperfection <kontakt.apperfection@gmail.com>
2013-09-25 04:40:16 +0200

    xxx

--- js/index.js
+++ js/index.js
@@ -17,66 +17,20 @@ function animateEle(e) {
 
 	if ($(this).index() == previousIndex) {} else {
 
+	href = ("index.html " + $(this).data('page'));
+	$(".panelButton").off('click');
+	$("#loads").css('z-index', 20);
 	$(".panelButton").removeClass("theChoosenOne");
 	$(this).addClass("theChoosenOne");
-
-	hrefPage = ("content/" + $(this).data('page'));
-	hrefScipt = ("js/" + $(this).data('script'));
-
 	wantedPosition = $(this).position();
 	nextCoordinates = ("0," + (wantedPosition.top)  + "px,0");
 	$("#currentArrow").anim({translate3d: nextCoordinates}, 0.6, 'ease-out');
-
-	front = $(".front");
-	back = $(".back");
-	$(".panelButton").off('click');
-
-	if($(this).index() > previousIndex) { //lower
-		previousIndex = $(this).index();
-		//$(back).load(hrefPage, function(){
-			$(back).append(document.getElementById( "gal" ));
-			$(back).anim({translate3d: '0,100%,0'}, 0, 'none', function(){
-				$(front).css('z-index', 9);
-				$(back).css('z-index', 10);
-				$(back).anim({translate3d: '0,0,0'}, 0.6, 'ease-out');
-				$(front).anim({translate3d: '0,-100%,0'}, 0.6, 'ease-out', function(){
-					$(front).anim({translate3d: '0,0,0'}, 0, 'none');
-					$(front).removeClass("front");
-					$(front).addClass("back");
-					$(back).removeClass("back");
-					$(back).addClass("front");
-					$(front).append(document.getElementById( "preload" ));
-					$(".panelButton").on('click', animateEle);
-					$.getScript(hrefScipt);
-				});
-			});
-		//});
-	}
-
-	else { //higher
-		previousIndex = $(this).index();
-			//$(back).load(hrefPage, function(){
-			$(back).append(document.getElementById( "sp" ));
-			$(back).anim({translate3d: '0,-100%,0'}, 0, 'none', function(){
-				$(front).css('z-index', 9);
-				$(back).css('z-index', 10);
-				$(back).anim({translate3d: '0,0,0'}, 0.6, 'ease-out');
-				$(front).anim({translate3d: '0,100%,0'}, 0.6, 'ease-out', function(){
-					$(front).anim({translate3d: '0,0,0'}, 0, 'none');
-					$(front).removeClass("front");
-					$(front).addClass("back");
-					$(back).removeClass("back");
-					$(back).addClass("front");
-					$(front).append(document.getElementById( "preload" ));
-					$(".panelButton").on('click', animateEle);
-					$.getScript(hrefScipt);
-				});
-			});
-		//});
-		} 
-	}
-
-}
+	$( ".front" ).load( href , function(){
+	//$("#loads").css("visibility", "hidden");
+	$(".panelButton").on('click', animateEle);
+	$("#loads").css('z-index', -10);
+	} );
+}}
 //end of handler
 
 });
